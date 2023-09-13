0e52f482d0ba4dc070af1b322ab21e46b5a597bb
NT <128177967+noto-21@users.noreply.github.com>
2023-09-12 22:45:00 -0400

    Slight adjustments to 'UI'

--- src/SGDO/GarageDoorDriver.java
+++ src/SGDO/GarageDoorDriver.java
@@ -8,10 +8,10 @@ public class GarageDoorDriver
 
 	public void openDoor(int speedLevel)
 	{
-		log.info("Garage Door Opening at " + speedLevel + " speed");
+		log.info("Garage Door Opening at a speed-level of: " + speedLevel + "\n");
 	}
 	public void closeDoor(int speedLevel)
 	{
-		log.info("Garage Door Closing " + speedLevel + " speed");
+		log.info("Garage Door Closing at a speed-level of: " + speedLevel + "\n");
 	}
 }
